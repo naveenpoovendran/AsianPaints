472f89b403bac6ba8cd6e2741530c358c4dd0267
pnaveen3 <pnaveen3@LIN76001903.corp.capgemini.com>
2021-05-07 11:45:02 +0530

    AsianPaints

--- AsianPaints/src/test/java/runner/testRunner.java
+++ AsianPaints/src/test/java/runner/testRunner.java
@@ -1,9 +1,9 @@
-package testRunner;
+package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features="src/test/resources/Features/Shop.feature",glue= {"stepDefinition"})
+@CucumberOptions(features="src/test/resources/Features/Shop.feature",glue= {"stepDefinition"},plugin= {"pretty","html:target/Information"})
 public class testRunner extends AbstractTestNGCucumberTests 
 {
 
